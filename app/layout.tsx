e84c1c4abab36e7136be73de411359b7f16b6104
garvsl <garvsehgal7@gmail.com>
2024-12-22 20:18:17 -0500

    swap fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ const garvFont = localFont({
   src: [
     {
       path: "../public/fonts/idk.woff2",
-      weight: "300",
+      weight: "400",
     },
     {
       path: "../public/fonts/normal.woff2",
-      weight: "400",
+      weight: "300",
     },
     {
       path: "../public/fonts/mono.woff",
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${garvFont.variable}  font-garvFont   antialiased bg-[#faf9f8] text-[rgba(0,0,0,0.85)]`}
+        className={`${garvFont.variable}  font-garvFont mix-blend-normal   antialiased bg-[#faf9f8] text-[rgba(0,0,0,0.85)]`}
       >
         {children}
         <Analytics />
